71cc68e222edf660be2640e617aa4ea33ac786ef
John Lombard <john.lombard@wyyerd.com>
2019-10-01 10:16:05 -0400

    semantic and faker

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+
 
 const App = () => {
-    return <div>Hello World</div>;
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt='avatar' src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className='author'>
+                        Test
+                    </a>
+                </div>
+                <div className="metadata">
+                    <span className="date">
+                        Today
+                    </span>
+                </div>
+                <div className="text">
+                    Nice bblog posts
+                </div>
+            </div>
+        </div>
+    )
 };
 
 ReactDOM.render(
