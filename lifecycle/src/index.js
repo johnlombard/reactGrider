1b759a0875e36f2b77f7f502ef973ae9da213b25
John Lombard <john.lombard@wyyerd.com>
2019-10-07 10:27:46 -0400

    Handling errors and conditional rendering

--- lifecycle/src/index.js
+++ lifecycle/src/index.js
@@ -2,13 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component  {
-    render() {
+    constructor(props){
+        // refrence to parents constructor function
+        super(props);
+
+        this.state = {lat: null, errorMessage: '' };
+
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
+            position => {
+                this.setState({lat: position.coords.latitude})
+            },
+            err => {
+                this.setState({ errorMessage: err.message})
+            },
         );
-    
-        return <div>Latitude: </div>
+    }
+
+    render() {  
+        // return <div>Latitude: {this.state.lat}</div>
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+       return <div>Loading!</div>
     }
 }
 
