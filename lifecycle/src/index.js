f6d02c320dc29f5b93d206d28e39ba966ac61e82
John Lombard <john.lombard@wyyerd.com>
2019-10-08 10:55:47 -0400

    Icons and season display

--- lifecycle/src/index.js
+++ lifecycle/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component  {
     constructor(props){
@@ -7,7 +8,9 @@ class App extends React.Component  {
         super(props);
 
         this.state = {lat: null, errorMessage: '' };
+    }
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({lat: position.coords.latitude})
@@ -25,7 +28,7 @@ class App extends React.Component  {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
        return <div>Loading!</div>
