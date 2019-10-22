72c7862eb6750a7d493412519005f058af8799c7
John Lombard <john.lombard@wyyerd.com>
2019-10-22 19:36:13 -0400

    finished app

--- lifecycle/src/index.js
+++ lifecycle/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component  {
     constructor(props){
@@ -22,7 +23,6 @@ class App extends React.Component  {
     }
 
     render() {  
-        // return <div>Latitude: {this.state.lat}</div>
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
@@ -30,8 +30,7 @@ class App extends React.Component  {
         if (!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />
         }
-
-       return <div>Loading!</div>
+       return <Spinner message="Please accept location request" />
     }
 }
 
