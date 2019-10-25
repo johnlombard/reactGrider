39058b3601876865307d455ced077f201bff5dd0
John Lombard <john.lombard@wyyerd.com>
2019-10-25 13:52:57 -0400

    image search complete

--- input/src/components/App.js
+++ input/src/components/App.js
@@ -3,7 +3,6 @@ import Unsplash from '../api/Unsplash';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList'
 
-
 class App extends React.Component {
     state = { images: []}
 
