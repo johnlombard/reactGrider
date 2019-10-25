63d5662deca25e0135cd8c130d61051efbb62b6a
John Lombard <john.lombard1996@gmail.com>
2019-10-25 15:08:08 -0400

    check email linked

--- input/src/components/ImageCard.js
+++ input/src/components/ImageCard.js
@@ -3,7 +3,6 @@ import React from 'react';
 class ImageCard extends React.Component {
     constructor(props) {
         super(props);
-
         this.state={spans: 0}
         this.imageRef = React.createRef();
     }
